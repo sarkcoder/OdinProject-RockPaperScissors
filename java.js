71eed9278349197829a76e95207ecb9971f06cfa
sarkcoder <95859784+sarkcoder@users.noreply.github.com>
2024-01-05 12:25:01 +0000

    Add overall winner to loop

--- java.js
+++ java.js
@@ -59,11 +59,29 @@ else{
 }
 
 function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
     console.log("Game Start")
     for (let i = 0; i < 5; i++){
         const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
+        if (checkWinner(playerSelection, computerSelection) == "Player Wins"){
+            scorePlayer++;
+        }
+        else if (checkWinner(playerSelection, computerSelection) == "Computer Wins"){
+            scoreComputer++;
+        }
+        }
+    console.log("Game Ends");
+    if(scorePlayer > scoreComputer){
+        console.log("Player was the winner!")
+    }
+    else if(scorePlayer < scoreComputer){
+        console.log("Computer was the winner")
+    }
+    else{
+        console.log("Its a tie!")
     }
 }
 // Debugging
