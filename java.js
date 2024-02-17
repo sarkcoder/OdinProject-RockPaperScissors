dba7542d2d05db00f2cc942351b546fa0b372f7a
sarkcoder <95859784+sarkcoder@users.noreply.github.com>
2024-02-17 13:52:12 +0000

    Tidy up code

--- java.js
+++ java.js
@@ -1,10 +1,12 @@
-// Declarations
+// Declarations and connect elements to DOM
+
 const array = ["rock", "paper", "scissors"];
 const buttons = document.querySelectorAll('button');
 const resulttext = document.getElementById('results')
 const winnertext = document.getElementById('winner')
 const scoretext = document.getElementById('scores')
 
+//establish baseline scores
 
 let scorePlayer = 0;
 let scoreComputer = 0;
@@ -22,31 +24,31 @@ function playRound(playerSelection){
     let result = ""
     let computerSelection = getComputerChoice();
     
-    if      (playerSelection == computerSelection){
-            result = `Tie, you both chose ${playerSelection}`
-            
+    if  (playerSelection == computerSelection){
+        result = `Tie, you both chose ${playerSelection}`  
     }
 
-    else if ((playerSelection == "rock" && computerSelection == "scissors") ||
-            (playerSelection == "paper" && computerSelection == "rock") ||
-            (playerSelection == "scissors" && computerSelection == "paper")) {
-            scorePlayer ++
-            result = `You win! ${playerSelection} beats ${computerSelection}`
-            
-
+    else if 
+        ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+        scorePlayer ++
+        result = `You win! ${playerSelection} beats ${computerSelection}`
     }
     
-    else   {
-            scoreComputer ++
-            result = `You lose! ${computerSelection} beats ${playerSelection}`
-            
+    else{
+        scoreComputer ++
+        result = `You lose! ${computerSelection} beats ${playerSelection}`  
     }    
-            resulttext.textContent = result
-            scoretext.textContent = `Your score : ${scorePlayer} Computer's score ${scoreComputer}`
+        // send results to DOM
+        resulttext.textContent = result
+        scoretext.textContent = `Your score : ${scorePlayer} Computer's score ${scoreComputer}`
+        trackwinner()
             
             
 }
 
+// tract who is first to 5 points and send results to DOM
 function trackwinner(){
 
     let winner = ""
@@ -62,26 +64,19 @@ function trackwinner(){
     else {winner = "continue"}
 
     winnertext.textContent = winner
-
-
 }
 
 // Disable buttons if player or computer reaches 5 points
 function endGame(){
-    buttons.forEach(elem => {
-        elem.disabled = true
-    })
+        buttons.forEach(elem => {
+        elem.disabled = true})
 }
 
 buttons.forEach(button =>{
         button.addEventListener('click', function(){
-    
         playRound(button.id)
-        trackwinner()
-        
         //debugging 
         //console.log (`Player Score = ${scorePlayer}`)
         //console.log (`Computer Score = ${scoreComputer}`)
 })
-
 })
